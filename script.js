afdceb73186b0c058335a8e475f31ba32226dd55
yashdeep singh <gosalyashdeep51@gmail.com>
2026-03-17 21:17:43 +0530

    practice about primitive data type

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-console.log("HELLO WORLD");
-document.write("This is my first javascript page");
+//console.log("HELLO WORLD");
+//document.write("This is my first javascript page");
+
+//javascript primitive values type
+
+//number
+var num=10;
+console.log(num);
+console.log(typeof(num));
+
+//boolean
+num=false;
+console.log(num);
+console.log(typeof(num));
+
+//null
+num=null;
+console.log(num);
+console.log(typeof(num));
+
+//undefined
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+
+//string
+num="Yashdeep";
+console.log(num);
+console.log(typeof(num));
