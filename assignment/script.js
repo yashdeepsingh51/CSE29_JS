f6d528cf22e4464872146dca360a8a84fcb77390
yashdeep singh <gosalyashdeep51@gmail.com>
2026-03-18 21:58:05 +0530

    learn about javascript arithmetic operators

---
+++ assignment/script.js
@@ -0,0 +1,24 @@
+//javascript arithmetic operators
+
+var a=100; var b=13; var c=10; var linebreak="<br>";
+
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
+document.write("a+b=");
+result=a+b;
+document.write(result);
+document.write(linebreak);
+document.write("a-b=");
+result=a-b;
+document.write(result);
+document.write(linebreak);
+document.write("a/b=");
+result=a/b;
+document.write(result);
+document.write(linebreak);
+document.write("a%b=");
+result=a%b;
+document.write(result);
+document.write(linebreak);
