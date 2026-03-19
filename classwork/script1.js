e42149398dc18e8c5e74eef9d1d090ad93bb8b2a
yashdeep singh <gosalyashdeep51@gmail.com>
2026-03-19 18:41:06 +0530

    learned about loop

---
+++ classwork/script1.js
@@ -0,0 +1,7 @@
+ var count;
+ document.write("starting loop"+"<br/>");
+ for(count=0;count<10;count++){
+    document.write("currentcount:"+count);
+   document.write("<br/>");
+ }
+    document.write("loop stopped");
