dbf7475be7a30297024b73836bb23880978180fe
yashdeep singh <gosalyashdeep51@gmail.com>
2026-04-04 21:24:10 +0530

    updated js file

--- experiment1/script.js
+++ experiment1/script.js
@@ -3,28 +3,35 @@ function calculateResult(){
     let n=document.getElementById("subject").value;
     let i;
     let total=0;
-    for(i=0;i<n;i++){
+    let result;
+    for(i=1;i<=n;i++){
 
-        let x=parseFloat(prompt("enter the marks of Subject"+(i+1)));
+        let x=parseFloat(prompt("enter the marks of Subject"+i));
 
         total+=x;
     }
     let average=total/n;
     let grade;
-    if(average>90)
-      grade='A';
-    else if(average>80)
+    if(average>=90)
+      grade='A+';
+    else if(average>=80)
+        grade='A';
+    else if(average>=70)
+        grade='B+';
+    else if(average>=60)
         grade='B';
-    else if(average>70)
+    else if(average>=50)
         grade='C';
-    else if(average>60)
-        grade='D';
-    else if(average>50)
-        grade='E';
     else
         grade='F';
 
 
+    if(average>=50)
+        result="Pass";
+    else
+        result="Fail";
 
-    let result=document.getElementById("result").innerHTML="Total Marks"+total+"<br/>"+"Average Marks"+average+"<br/>"
+
+
+    document.getElementById("result").innerHTML="Total Marks:"+total+"<br/>"+"Average Marks:"+average+"<br/>"+"Grade:"+grade+"<br/>"+"Result:"+result;
 }
