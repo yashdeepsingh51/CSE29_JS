24a7125d110d24e3e59c540b43a165a28337b9a0
yashdeep singh <gosalyashdeep51@gmail.com>
2026-04-22 19:54:02 +0530

    firstapp of react created

---
+++ react/firstapp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import student from './component/Student.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ react/firstapp/src/component/Student.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Student({name,age}){
+    return(
+        <div>
+            <h1>{name}</h1>
+            <br/>
+            <h3>{age}</h3>
+        </div>
+    )
+}
+
+export default "react";
